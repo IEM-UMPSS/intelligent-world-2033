f128e99f64c730952012d3e0991250181c30f26a
unknown <hovahyii@gmail.com>
2023-04-22 00:44:12 +0800

    edit bug:
    Signed-off-by: unknown <hovahyii@gmail.com>

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -34,15 +34,9 @@ const LandingPage: NextPage = () => {
           <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 md:gap-8">
             <Link
               className="flex max-w-xs flex-col gap-4 rounded-xl bg-black/10 p-4 text-black hover:bg-black/20"
-              href="#register"
+              href="https://forms.gle/nbGZ7E6JJQMpbbmR8"
             >
-              <Link
-                className="flex max-w-xs flex-col gap-4 rounded-xl bg-black/10 p-4 text-black hover:bg-black/20"
-                href="https://forms.gle/nbGZ7E6JJQMpbbmR8"
-                target="_blank"
-              >
-                <h3 className="text-2xl font-bold">Register now →</h3>
-              </Link>
+              <h3 className="text-2xl font-bold">Register now →</h3>
               <div className="text-lg">
                 Join us at Huawei Day and learn about the Intelligent World
                 2030.
