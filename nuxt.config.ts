49e1e400e0143909f2180fbc2ba7bb3f08ed9628
unknown <hovahyii@gmail.com>
2023-05-04 02:06:29 +0800

    migrate to element plus
    Signed-off-by: unknown <hovahyii@gmail.com>

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,12 +1,6 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   modules: [
-    '@vuestic/nuxt'
+   
   ],
-
-  vuestic: {
-    config: {
-      // Vuestic config here
-    }
-  },
 })
